d45b5b871ca72d95714c43ef16d9a4d1a6be0168
appuser <appuser@journey2-679f96f5bd-r8m44>
2024-08-05 09:18:15 +0000

    starter project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using DungeonCrawl.Ui;
+using SadConsole;
+using SadConsole.Configuration;
+
+namespace DungeonCrawl;
+
+/// <summary>
+/// Class <c>Program</c> provides an entry point for the game.
+/// </summary>
+public static class Program
+{
+    private const int ViewPortWidth = 80;
+    private const int ViewPortHeight = 25;
+
+    /// <summary>
+    /// The entry point of the program.
+    /// </summary>
+    public static void Main()
+    {
+        // Create startup configuration for the engine.
+        Builder startup = new Builder()
+            .SetScreenSize(ViewPortWidth, ViewPortHeight)
+            .SetStartingScreen<RootScreen>()
+            .IsStartingScreenFocused(true);
+
+        Game.Create(startup);
+        Game.Instance.Run();
+        Game.Instance.Dispose();
+    }
+}
